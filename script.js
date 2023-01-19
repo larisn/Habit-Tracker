a46b4b42d1c4bb4c04622dd6b75defa6e4f788bb
Larissa <contatolarisn@gmail.com>
2023-01-19 13:00:22 -0300

    adicionando javascript no projeto

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup (form)
+
+const data = {
+    run: ['01-01', '01-02', '01-03'],
+}
+
+nlwSetup.setData(data) 
+nlwSetup.load()
+
 
