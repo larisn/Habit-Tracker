dd52f81ceb7202820791a4512c546527107f8519
Larissa <contatolarisn@gmail.com>
2023-01-20 16:27:42 -0300

    adicionando comandos

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const form = document.querySelector('#form-habits')
-const nlwSetup = new NLWSetup (form)
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-    run: ['01-01', '01-02', '01-03'],
+button.addEventListener("click", add) /* adicionar funçao no botao */
+form.addEventListener("change", save) /* salvar os dias */
+
+function add() {
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5) /* slice ''recorta'' os dados */
+    const dayExists = nlwSetup.dayExists(today)
+    
+    if(dayExists) {
+        alert("Erro: Esse dia já foi adicionado ❌")
+        return /* para o código imediatamente */
+    }
+
+    alert('Dia adicionado com sucesso! ✔')
+    nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data) 
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data)) /* JSON transforma os dados em string */
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
 nlwSetup.load()
-
-
